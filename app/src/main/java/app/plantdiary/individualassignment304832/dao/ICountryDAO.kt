0c0432e1bd63cd3a32b7a0313dabc00e0ba2c936
grantkollar <89742140+grantkollar@users.noreply.github.com>
2023-02-24 15:20:38 -0500

    3/4 tests working

--- app/src/main/java/app/plantdiary/individualassignment304832/dao/ICountryDAO.kt
+++ app/src/main/java/app/plantdiary/individualassignment304832/dao/ICountryDAO.kt
@@ -5,6 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface ICountryDAO {
-    @GET("/")
+    @GET("https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json")
     fun getAllCountries(): Call<ArrayList<Country>>
 }
--- app/src/main/java/app/plantdiary/individualassignment304832/service/CountryService.kt
+++ app/src/main/java/app/plantdiary/individualassignment304832/service/CountryService.kt
@@ -8,12 +8,17 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import retrofit2.awaitResponse
 
-class CountryService {
-    suspend fun fetchCountries(): List<Country>? {
+
+interface ICountryService{
+    suspend fun fetchCountries(): List<Country>?
+}
+class CountryService : ICountryService {
+
+    override suspend fun fetchCountries(): List<Country>? {
         return withContext(Dispatchers.IO) {
             val service = RetrofitClientInstance.retrofitInstance?.create(ICountryDAO::class.java)
-            val plants = async {service?.getAllCountries()}
-            var result = plants.await()?.awaitResponse()?.body()
+            val countries = async {service?.getAllCountries()}
+            var result = countries.await()?.awaitResponse()?.body()
             return@withContext result
 
         }
